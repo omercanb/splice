0f895906b254fd3fc47ca0d64f175c3a9cdb7e93
omercanb <omercanbaykara@gmail.com>
2026-08-20 11:59:41 +0300

    Add a fixed size array in the stdlib
    This needed some small modifications to mypy to work. Normally final ints couldnt be array sizes (ie TypeVars) in mypy, and only literals could be, but literals can't be used anywhere else, so changed my mypy for to allow it

--- src/splice/stdlib.py
+++ src/splice/stdlib.py
@@ -5,14 +5,53 @@ directly in the interpreter, not just when compiled.
 """
 
 from copy import deepcopy
-from typing import TypeVar
+from typing import Generic, TypeVar, get_args
 
-_T = TypeVar("_T")
+T = TypeVar("T")
+N = TypeVar("N", bound=int)
 
 
-def copy(value: _T) -> _T:
+def copy(value: T) -> T:
     """A deep copy used for mutable value semantics. Not translated directly to C++"""
     return deepcopy(value)
+
+
+class Array(Generic[T, N]):
+    def __class_getitem__(cls, params):
+        element_type, size = params
+        if get_args(size):
+            # If N is a literal[N] extract the number
+            # Otherwise its a final int
+            (size,) = get_args(size)
+        return _ArrayAlias(element_type, size)
+
+    def __init__(self, element_type, size):
+        self.data = [element_type() for _ in range(size)]
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __setitem__(self, index, item):
+        self.data[index] = item
+
+    def __len__(self):
+        return len(self.data)
+
+    def __str__(self):
+        return str(self.data)
+
+    def fill(self, item):
+        for i in range(len(self.data)):
+            self.data[i] = item
+
+
+class _ArrayAlias:
+    def __init__(self, element_type, size):
+        self.element_type = element_type
+        self.size = size
+
+    def __call__(self):
+        return Array(self.element_type, self.size)
 
 
 # Fixed-width integers, shaped like mypy_extensions' i64/i32/i16/u8.
